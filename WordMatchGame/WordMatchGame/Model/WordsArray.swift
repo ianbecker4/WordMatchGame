994f1021de05e1b9ed38ecd841719094741b6b1c
Ian Becker <ian-becker@lambdastudents.com>
2020-08-26 11:11:37 -0700

    Created model for our words

---
+++ WordMatchGame/WordMatchGame/Model/WordsArray.swift
@@ -0,0 +1,15 @@
+//
+//  WordsArray.swift
+//  WordMatchGame
+//
+//  Created by Ian Becker on 8/26/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import Foundation
+
+class WordsArray {
+    
+    let words: [String] = []
+    
+}
